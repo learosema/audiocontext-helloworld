55d4713e5af9a0d6a52620cf16782ae7ca0604e3
Lutz Rosema <info@terabaud.de>
2015-04-01 18:31:43 +0200

    reuse AudioContext; added createBuffer experiment

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-
-
-test.addEventListener("click", function (e) {
-	var ac = new AudioContext();
+var ac = new AudioContext();
+oscillatorPlayButton.onclick = function (e) {
 	var osc = ac.createOscillator();
-	var gain = ac.createGain();
-	var bq = ac.createBiquadFilter();
-
+	var gain = ac.createGain();  
+    var duration = parseInt(dura.value) * 1000;
 	osc.connect(gain);
 	gain.connect(ac.destination);
 	osc.type = wave.value;
 	osc.frequency.value = freq.value;
 	gain.gain.value = vol.value;
 	osc.start();
-	osc.stop(2);
-});
+    window.setTimeout(function () {
+        osc.stop();
+        osc.disconnect();
+    }, duration);
+};
 
-
+whiteNoiseButton.onclick = function (e) {
+    var frames = ac.sampleRate;
+    var buf = ac.createBuffer(2, frames, ac.sampleRate);
+    for (var ch = 0; ch < 2; ch++) {
+        var cData = buf.getChannelData(ch);
+        for (var i = 0; i < frames; i++) {
+            cData[i] = Math.random() * 2 - 1;
+        }
+    }
+    var src = ac.createBufferSource();
+    src.buffer = buf;
+    src.connect(ac.destination);
+    src.start();
+}
