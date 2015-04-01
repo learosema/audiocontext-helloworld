9efa9f2585761ab8798ddb957f07f5a68dde2505
Lutz Rosema <info@terabaud.de>
2015-04-01 19:08:12 +0200

    added createWaveShaper experiment

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 var ac = new AudioContext();
+
+function makeDistortionCurve(amount) {
+  var k = typeof amount === 'number' ? amount : 50,
+    n_samples = 44100,
+    curve = new Float32Array(n_samples),
+    deg = Math.PI / 180,
+    i = 0,
+    x;
+  for ( ; i < n_samples; ++i ) {
+    x = i * 2 / n_samples - 1;
+    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+  }
+  return curve;
+}
+
 oscillatorPlayButton.onclick = function (e) {
     var osc = ac.createOscillator();
     var gain = ac.createGain();  
-    var duration = parseInt(dura.value) * 1000;
-    osc.connect(gain);
+    var distortion = ac.createWaveShaper();
+    distortion.curve = makeDistortionCurve(parseInt(dist.value));
+    distortion.oversample = "4x";
+    osc.connect(distortion);
+    distortion.connect(gain);
     gain.connect(ac.destination);
     osc.type = wave.value;
     osc.frequency.value = freq.value;
     gain.gain.value = vol.value;
     osc.start();
+    var duration = parseInt(dura.value) * 1000;
     window.setTimeout(function () {
         osc.stop();
         osc.disconnect();
@@ -29,3 +48,4 @@ whiteNoiseButton.onclick = function (e) {
     src.connect(ac.destination);
     src.start();
 };
+
